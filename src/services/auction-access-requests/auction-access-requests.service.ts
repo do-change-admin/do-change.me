5d6f94a3de809281596d58a307e987314c5c4ad6
alevny <alevnyacow@gmail.com>
2025-09-16 17:45:39 +0400

    update login in service

--- src/services/auction-access-requests/auction-access-requests.service.ts
+++ src/services/auction-access-requests/auction-access-requests.service.ts
@@ -3,6 +3,7 @@ import { AdminUpdateAuctionAccessRequest, AuctionAccessRequestListItem, AuctionA
 import { prismaClient } from "@/infrastructure";
 import { ProfileService } from "../profile";
 import { EmailAddress } from "@/value-objects/email-address.vo";
+import { businessError } from "@/lib/errors";
 
 type FindListQueryData = PaginationSchemaType & { status: AuctionAccessRequestStatus }
 
@@ -38,10 +39,45 @@ export class AuctionAccessRequestsService {
     }
 
     adminUpdate = async (payload: AdminUpdateAuctionAccessRequest) => {
+        const accordingRequest = await prismaClient.auctionAccessRequest.findUnique({ where: { id: payload.id } })
+        if (!accordingRequest) {
+            throw businessError('No according request was found')
+        }
 
+        const steps: Partial<Record<AuctionAccessRequestStatus, AuctionAccessRequestStatus>> = {
+            'review': 'awaiting user confirmation',
+            'awaiting user confirmation': 'call scheduling',
+            'call scheduling': 'call completed',
+            'awaiting documents upload': 'documents under review',
+            'documents under review': 'corrections required',
+            'corrections required': 'ready for auction access',
+            'ready for auction access': 'approved'
+        }
+
+        await prismaClient.auctionAccessRequest.update({
+            where: { id: payload.id },
+            data: {
+                timeSlots: payload.availableTimeSlots ? {
+                    createMany: {
+                        data: payload.availableTimeSlots
+                    }
+                } : undefined,
+                status: payload.progress ? payload.progress === 'next approve step' ? steps[accordingRequest.status as AuctionAccessRequestStatus] || 'approved' : 'rejected' : undefined
+            },
+        })
     }
 
     update = async (payload: UpdateAuctionAccessRequest) => {
+        const foundRequest = await prismaClient.auctionAccessRequest.count({ where: { id: payload.id } })
+        if (!foundRequest) {
+            throw businessError('No according request was found')
+        }
+        await prismaClient.auctionAccessRequest.update({
+            where: { id: payload.id },
+            data: {
+                activeSlotId: payload.selectedTimeSlotId
+            },
+        })
 
     }
 }
