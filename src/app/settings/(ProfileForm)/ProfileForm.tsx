b3806b04203db14df5b49a17b0185751fa56a699
alevny <alevnyacow@gmail.com>
2025-09-27 17:49:19 +0400

    build fix

--- src/app/settings/(ProfileForm)/ProfileForm.tsx
+++ src/app/settings/(ProfileForm)/ProfileForm.tsx
@@ -1,69 +1,69 @@
 'use client'
 
-import React, {ChangeEvent, FC, FormEvent, useEffect, useState} from 'react';
+import React, { ChangeEvent, FC, FormEvent, useEffect, useState } from 'react';
 import styles from "./ProfileForm.module.css";
-import {Avatar, Button, Loader, Select, Text} from "@mantine/core";
-import {FaCamera} from "react-icons/fa";
-import {useProfile, useProfileModifying, useUploadPhoto} from "@/hooks";
-import {notifications} from "@mantine/notifications";
-import {ProfileFormSkeleton} from "@/app/settings/(ProfileForm)/ProfileFormSkeleton";
-import {DateInput} from '@mantine/dates';
-import {AiOutlineExclamationCircle} from "react-icons/ai";
+import { Avatar, Button, Loader, Select, Text } from "@mantine/core";
+import { FaCamera } from "react-icons/fa";
+import { useProfile, useProfileModifying, useUploadPhoto } from "@/hooks";
+import { notifications } from "@mantine/notifications";
+import { ProfileFormSkeleton } from "@/app/settings/(ProfileForm)/ProfileFormSkeleton";
+import { DateInput } from '@mantine/dates';
+import { AiOutlineExclamationCircle } from "react-icons/ai";
 
 export const STATES = [
-    {value: "AL", label: "Alabama (AL)"},
-    {value: "AK", label: "Alaska (AK)"},
-    {value: "AZ", label: "Arizona (AZ)"},
-    {value: "AR", label: "Arkansas (AR)"},
-    {value: "CA", label: "California (CA)"},
-    {value: "CO", label: "Colorado (CO)"},
-    {value: "CT", label: "Connecticut (CT)"},
-    {value: "DE", label: "Delaware (DE)"},
-    {value: "FL", label: "Florida (FL)"},
-    {value: "GA", label: "Georgia (GA)"},
-    {value: "HI", label: "Hawaii (HI)"},
-    {value: "ID", label: "Idaho (ID)"},
-    {value: "IL", label: "Illinois (IL)"},
-    {value: "IN", label: "Indiana (IN)"},
-    {value: "IA", label: "Iowa (IA)"},
-    {value: "KS", label: "Kansas (KS)"},
-    {value: "KY", label: "Kentucky (KY)"},
-    {value: "LA", label: "Louisiana (LA)"},
-    {value: "ME", label: "Maine (ME)"},
-    {value: "MD", label: "Maryland (MD)"},
-    {value: "MA", label: "Massachusetts (MA)"},
-    {value: "MI", label: "Michigan (MI)"},
-    {value: "MN", label: "Minnesota (MN)"},
-    {value: "MS", label: "Mississippi (MS)"},
-    {value: "MO", label: "Missouri (MO)"},
-    {value: "MT", label: "Montana (MT)"},
-    {value: "NE", label: "Nebraska (NE)"},
-    {value: "NV", label: "Nevada (NV)"},
-    {value: "NH", label: "New Hampshire (NH)"},
-    {value: "NJ", label: "New Jersey (NJ)"},
-    {value: "NM", label: "New Mexico (NM)"},
-    {value: "NY", label: "New York (NY)"},
-    {value: "NC", label: "North Carolina (NC)"},
-    {value: "ND", label: "North Dakota (ND)"},
-    {value: "OH", label: "Ohio (OH)"},
-    {value: "OK", label: "Oklahoma (OK)"},
-    {value: "OR", label: "Oregon (OR)"},
-    {value: "PA", label: "Pennsylvania (PA)"},
-    {value: "RI", label: "Rhode Island (RI)"},
-    {value: "SC", label: "South Carolina (SC)"},
-    {value: "SD", label: "South Dakota (SD)"},
-    {value: "TN", label: "Tennessee (TN)"},
-    {value: "TX", label: "Texas (TX)"},
-    {value: "UT", label: "Utah (UT)"},
-    {value: "VT", label: "Vermont (VT)"},
-    {value: "VA", label: "Virginia (VA)"},
-    {value: "WA", label: "Washington (WA)"},
-    {value: "WV", label: "West Virginia (WV)"},
-    {value: "WI", label: "Wisconsin (WI)"},
-    {value: "WY", label: "Wyoming (WY)"},
+    { value: "AL", label: "Alabama (AL)" },
+    { value: "AK", label: "Alaska (AK)" },
+    { value: "AZ", label: "Arizona (AZ)" },
+    { value: "AR", label: "Arkansas (AR)" },
+    { value: "CA", label: "California (CA)" },
+    { value: "CO", label: "Colorado (CO)" },
+    { value: "CT", label: "Connecticut (CT)" },
+    { value: "DE", label: "Delaware (DE)" },
+    { value: "FL", label: "Florida (FL)" },
+    { value: "GA", label: "Georgia (GA)" },
+    { value: "HI", label: "Hawaii (HI)" },
+    { value: "ID", label: "Idaho (ID)" },
+    { value: "IL", label: "Illinois (IL)" },
+    { value: "IN", label: "Indiana (IN)" },
+    { value: "IA", label: "Iowa (IA)" },
+    { value: "KS", label: "Kansas (KS)" },
+    { value: "KY", label: "Kentucky (KY)" },
+    { value: "LA", label: "Louisiana (LA)" },
+    { value: "ME", label: "Maine (ME)" },
+    { value: "MD", label: "Maryland (MD)" },
+    { value: "MA", label: "Massachusetts (MA)" },
+    { value: "MI", label: "Michigan (MI)" },
+    { value: "MN", label: "Minnesota (MN)" },
+    { value: "MS", label: "Mississippi (MS)" },
+    { value: "MO", label: "Missouri (MO)" },
+    { value: "MT", label: "Montana (MT)" },
+    { value: "NE", label: "Nebraska (NE)" },
+    { value: "NV", label: "Nevada (NV)" },
+    { value: "NH", label: "New Hampshire (NH)" },
+    { value: "NJ", label: "New Jersey (NJ)" },
+    { value: "NM", label: "New Mexico (NM)" },
+    { value: "NY", label: "New York (NY)" },
+    { value: "NC", label: "North Carolina (NC)" },
+    { value: "ND", label: "North Dakota (ND)" },
+    { value: "OH", label: "Ohio (OH)" },
+    { value: "OK", label: "Oklahoma (OK)" },
+    { value: "OR", label: "Oregon (OR)" },
+    { value: "PA", label: "Pennsylvania (PA)" },
+    { value: "RI", label: "Rhode Island (RI)" },
+    { value: "SC", label: "South Carolina (SC)" },
+    { value: "SD", label: "South Dakota (SD)" },
+    { value: "TN", label: "Tennessee (TN)" },
+    { value: "TX", label: "Texas (TX)" },
+    { value: "UT", label: "Utah (UT)" },
+    { value: "VT", label: "Vermont (VT)" },
+    { value: "VA", label: "Virginia (VA)" },
+    { value: "WA", label: "Washington (WA)" },
+    { value: "WV", label: "West Virginia (WV)" },
+    { value: "WI", label: "Wisconsin (WI)" },
+    { value: "WY", label: "Wyoming (WY)" },
 ];
 
-export const ProfileForm: FC<{ isNotSettings?: boolean }> = ({isNotSettings = false}) => {
+export const ProfileForm: FC<{ isNotSettings?: boolean }> = ({ isNotSettings = false }) => {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [phone, setPhone] = useState("");
@@ -72,9 +72,9 @@ export const ProfileForm: FC<{ isNotSettings?: boolean }> = ({isNotSettings = fa
     const [address, setAddress] = useState<string | null>(null)
     const [state, setState] = useState<string | null>(null)
     const [zipCode, setZipCode] = useState<string | null>(null)
-    const {data: profileData, isLoading: profileIsLoading} = useProfile();
-    const {mutate: modifyProfile, isPending: profileIsModifying} = useProfileModifying();
-    const {mutate: uploadPhoto, isPending: isPendingUploadPhoto} = useUploadPhoto();
+    const { data: profileData, isLoading: profileIsLoading } = useProfile();
+    const { mutate: modifyProfile, isPending: profileIsModifying } = useProfileModifying();
+    const { mutate: uploadPhoto, isPending: isPendingUploadPhoto } = useUploadPhoto();
 
     const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
         if (!event.target.files?.length) return;
@@ -103,7 +103,7 @@ export const ProfileForm: FC<{ isNotSettings?: boolean }> = ({isNotSettings = fa
     const handleSave = (e: FormEvent) => {
         e.preventDefault();
         modifyProfile(
-            {body: {bio, firstName, lastName, phone, birthDate: birthDate!, address, state, zipCode}},
+            { body: { bio, firstName, lastName, phone, birthDate: birthDate!, address, state, zipCode, auctionAccessNumber: null, auctionAccessQRLink: null } },
             {
                 onSuccess: () => {
                     notifications.show({
@@ -124,7 +124,7 @@ export const ProfileForm: FC<{ isNotSettings?: boolean }> = ({isNotSettings = fa
     };
 
     if (profileIsLoading || isPendingUploadPhoto || profileIsModifying) {
-        return <ProfileFormSkeleton/>
+        return <ProfileFormSkeleton />
     }
 
     return (
@@ -134,7 +134,7 @@ export const ProfileForm: FC<{ isNotSettings?: boolean }> = ({isNotSettings = fa
                 <Button
                     onClick={handleSave}
                     disabled={profileIsModifying}
-                    leftSection={profileIsModifying ? <Loader size="xs" color="white"/> : null}
+                    leftSection={profileIsModifying ? <Loader size="xs" color="white" /> : null}
                 >
                     {profileIsModifying ? 'Saving...' : 'Save'}
                 </Button>
@@ -147,7 +147,7 @@ export const ProfileForm: FC<{ isNotSettings?: boolean }> = ({isNotSettings = fa
                         className={styles.avatar}
                     />
                     <button className={styles.avatarBtn} aria-label="Change photo">
-                        <FaCamera className={styles.avatarIcon}/>
+                        <FaCamera className={styles.avatarIcon} />
                     </button>
                 </div>
 
@@ -157,10 +157,10 @@ export const ProfileForm: FC<{ isNotSettings?: boolean }> = ({isNotSettings = fa
                     <label htmlFor='photo-upload'>
                         <p className={styles.linkBtn}>Change Avatar</p>
                     </label>
-                    <input style={{display: 'none'}} id='photo-upload' type='file' onChange={handleFileChange}/>
+                    <input style={{ display: 'none' }} id='photo-upload' type='file' onChange={handleFileChange} />
                     {(isNotSettings && !Boolean(profileData?.photoLink)) && (
-                        <Text c="red" size="sm" style={{display: 'flex', alignItems: 'center', gap: 5}}>
-                            <AiOutlineExclamationCircle/>
+                        <Text c="red" size="sm" style={{ display: 'flex', alignItems: 'center', gap: 5 }}>
+                            <AiOutlineExclamationCircle />
                             This field is required
                         </Text>
                     )}
@@ -177,8 +177,8 @@ export const ProfileForm: FC<{ isNotSettings?: boolean }> = ({isNotSettings = fa
                         onChange={(e) => setFirstName(e.target.value)}
                     />
                     {(isNotSettings && !Boolean(profileData?.firstName)) && (
-                        <Text c="red" size="sm" style={{display: 'flex', alignItems: 'center', gap: 5}}>
-                            <AiOutlineExclamationCircle/>
+                        <Text c="red" size="sm" style={{ display: 'flex', alignItems: 'center', gap: 5 }}>
+                            <AiOutlineExclamationCircle />
                             This field is required
                         </Text>
                     )}
@@ -192,8 +192,8 @@ export const ProfileForm: FC<{ isNotSettings?: boolean }> = ({isNotSettings = fa
                         onChange={(e) => setLastName(e.target.value)}
                     />
                     {(isNotSettings && !Boolean(profileData?.lastName)) && (
-                        <Text c="red" size="sm" style={{display: 'flex', alignItems: 'center', gap: 5}}>
-                            <AiOutlineExclamationCircle/>
+                        <Text c="red" size="sm" style={{ display: 'flex', alignItems: 'center', gap: 5 }}>
+                            <AiOutlineExclamationCircle />
                             This field is required
                         </Text>
                     )}
@@ -213,8 +213,8 @@ export const ProfileForm: FC<{ isNotSettings?: boolean }> = ({isNotSettings = fa
                         p={0}
                     />
                     {(isNotSettings && !Boolean(profileData?.birthDate)) && (
-                        <Text c="red" size="sm" style={{display: 'flex', alignItems: 'center', gap: 5}}>
-                            <AiOutlineExclamationCircle/>
+                        <Text c="red" size="sm" style={{ display: 'flex', alignItems: 'center', gap: 5 }}>
+                            <AiOutlineExclamationCircle />
                             This field is required
                         </Text>
                     )}
@@ -230,8 +230,8 @@ export const ProfileForm: FC<{ isNotSettings?: boolean }> = ({isNotSettings = fa
                         disabled
                     />
                     {(isNotSettings && !Boolean(profileData?.email)) && (
-                        <Text c="red" size="sm" style={{display: 'flex', alignItems: 'center', gap: 5}}>
-                            <AiOutlineExclamationCircle/>
+                        <Text c="red" size="sm" style={{ display: 'flex', alignItems: 'center', gap: 5 }}>
+                            <AiOutlineExclamationCircle />
                             This field is required
                         </Text>
                     )}
@@ -246,8 +246,8 @@ export const ProfileForm: FC<{ isNotSettings?: boolean }> = ({isNotSettings = fa
                         onChange={(e) => setPhone(e.target.value)}
                     />
                     {(isNotSettings && !Boolean(profileData?.phone)) && (
-                        <Text c="red" size="sm" style={{display: 'flex', alignItems: 'center', gap: 5}}>
-                            <AiOutlineExclamationCircle/>
+                        <Text c="red" size="sm" style={{ display: 'flex', alignItems: 'center', gap: 5 }}>
+                            <AiOutlineExclamationCircle />
                             This field is required
                         </Text>
                     )}
@@ -263,8 +263,8 @@ export const ProfileForm: FC<{ isNotSettings?: boolean }> = ({isNotSettings = fa
                         onChange={(e) => setAddress(e.target.value)}
                     />
                     {(isNotSettings && !Boolean(profileData?.address)) && (
-                        <Text c="red" size="sm" style={{display: 'flex', alignItems: 'center', gap: 5}}>
-                            <AiOutlineExclamationCircle/>
+                        <Text c="red" size="sm" style={{ display: 'flex', alignItems: 'center', gap: 5 }}>
+                            <AiOutlineExclamationCircle />
                             This field is required
                         </Text>
                     )}
@@ -279,9 +279,9 @@ export const ProfileForm: FC<{ isNotSettings?: boolean }> = ({isNotSettings = fa
                         value={zipCode || ''}
                         onChange={(e) => setZipCode(e.target.value)}
                     />
-                    {(isNotSettings &&  !Boolean(profileData?.zipCode)) && (
-                        <Text c="red" size="sm" style={{display: 'flex', alignItems: 'center', gap: 5}}>
-                            <AiOutlineExclamationCircle/>
+                    {(isNotSettings && !Boolean(profileData?.zipCode)) && (
+                        <Text c="red" size="sm" style={{ display: 'flex', alignItems: 'center', gap: 5 }}>
+                            <AiOutlineExclamationCircle />
                             This field is required
                         </Text>
                     )}
@@ -302,9 +302,9 @@ export const ProfileForm: FC<{ isNotSettings?: boolean }> = ({isNotSettings = fa
                             input: styles.inputDateInput,
                         }}
                     />
-                    {(isNotSettings &&  !Boolean(profileData?.state)) && (
-                        <Text c="red" size="sm" style={{display: 'flex', alignItems: 'center', gap: 5}}>
-                            <AiOutlineExclamationCircle/>
+                    {(isNotSettings && !Boolean(profileData?.state)) && (
+                        <Text c="red" size="sm" style={{ display: 'flex', alignItems: 'center', gap: 5 }}>
+                            <AiOutlineExclamationCircle />
                             This field is required
                         </Text>
                     )}
@@ -319,8 +319,8 @@ export const ProfileForm: FC<{ isNotSettings?: boolean }> = ({isNotSettings = fa
                         onChange={(e) => setBio(e.target.value)}
                     />
                     {(isNotSettings && !Boolean(profileData?.bio)) && (
-                        <Text c="red" size="sm" style={{display: 'flex', alignItems: 'center', gap: 5}}>
-                            <AiOutlineExclamationCircle/>
+                        <Text c="red" size="sm" style={{ display: 'flex', alignItems: 'center', gap: 5 }}>
+                            <AiOutlineExclamationCircle />
                             This field is required
                         </Text>
                     )}
