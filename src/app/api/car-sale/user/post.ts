7acce3894067a25396e2705947105c21a9560f75
alevny <alevnyacow@gmail.com>
2025-10-07 23:57:43 +0400

    hotfix

--- src/app/api/car-sale/user/post.ts
+++ src/app/api/car-sale/user/post.ts
@@ -2,10 +2,19 @@ import { zodApiMethod, ZodAPIMethod, ZodAPISchemas } from "../../zod-api-methods
 import { getDIContainer } from "@/di-containers";
 import { CarSaleUserServiceFactory, ServiceTokens } from "@/di-containers/tokens.di-container";
 import { Services } from "@/services";
+import z from "zod";
 
 const schemas = {
     body: undefined,
-    query: Services.CarSaleUser.postCarPayloadSchema,
+    query: Services.CarSaleUser.postCarPayloadSchema.omit({
+        mileage: true,
+        price: true,
+        year: true
+    }).extend({
+        mileage: z.coerce.number(),
+        price: z.coerce.number(),
+        year: z.coerce.number()
+    }),
     response: undefined
 } satisfies ZodAPISchemas
 
