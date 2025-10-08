dd70f016cb773e0880fd55eb2a1029143011b3c0
Maksim Rylov <mksm.rlv.tb@gmail.com>
2025-10-08 22:13:54 +0300

    car adder

---
+++ src/components/CarAdder/CarAdder.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { FC } from "react";
+
+import { CarDraftAdder } from "./components/CarDraftAdder";
+import { CarNewAdder } from "./components/CarNewAdder";
+
+interface CarAdderProps {
+    opened: boolean;
+    onClose: () => void;
+    draftId?: string;
+}
+
+export const CarAdder: FC<CarAdderProps> = ({ onClose, opened, draftId }) => {
+    if (draftId) {
+        return (
+            <CarDraftAdder
+                draftId={draftId}
+                onClose={onClose}
+                opened={opened}
+            />
+        );
+    }
+
+    return <CarNewAdder onClose={onClose} opened={opened} />;
+};
--- src/hooks/_car-sale.user.hooks.tsx
+++ src/hooks/_car-sale.user.hooks.tsx
@@ -4,102 +4,171 @@ import { apiRequest, buildQueryString } from "@/lib/apiFetch";
 import type { Services } from "@/services";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
-type API = CarSaleUserAPI
-type DraftsAPI = CarSaleUserDraftDetailsAPI
+type API = CarSaleUserAPI;
+type DraftsAPI = CarSaleUserDraftDetailsAPI;
 
-const apiURL = '/api/car-sale/user'
-const draftsApiURL = '/api/car-sale/user/drafts'
+const apiURL = "/api/car-sale/user";
+const draftsApiURL = "/api/car-sale/user/drafts";
 
-export const useCarsForSaleUserList = (queryPayload: API['GET']['payload']['query']) => {
-    return useQuery<API['GET']['response'], API['GET']['error']>({
-        queryKey: ['cars-for-sale', 'user', 'list', queryPayload.status || 'all', queryPayload],
+export const useCarsForSaleUserList = (
+    queryPayload: API["GET"]["payload"]["query"]
+) => {
+    return useQuery<API["GET"]["response"], API["GET"]["error"]>({
+        queryKey: [
+            "cars-for-sale",
+            "user",
+            "list",
+            queryPayload.status || "all",
+            queryPayload,
+        ],
         queryFn: () => {
-            return apiRequest(apiURL, 'GET')({
-                query: queryPayload
-            } satisfies API['GET']['payload'])
-        }
-    })
-}
+            return apiRequest(
+                apiURL,
+                "GET"
+            )({
+                query: queryPayload,
+            } satisfies API["GET"]["payload"]);
+        },
+    });
+};
 
 export const useCarForSaleUserPosting = () => {
-    const queryClient = useQueryClient()
-    return useMutation<void, API['POST']['error'], Services.CarSaleUser.PostCarPayload>({
+    const queryClient = useQueryClient();
+    return useMutation<
+        void,
+        API["POST"]["error"],
+        Services.CarSaleUser.PostCarPayload
+    >({
         mutationFn: async (payload) => {
-            const formData = new FormData()
+            const formData = new FormData();
 
             for (const photo of payload.photos) {
-                formData.append('photos', photo)
+                formData.append("photos", photo);
             }
 
-            const { make, mileage, model, price, vin, year } = payload
-            await fetch(`${apiURL}${buildQueryString({ make, mileage, model, price, vin, year })}`, {
-                body: formData,
-                method: "POST"
-            })
+            const { make, mileage, model, price, vin, year } = payload;
+            await fetch(
+                `${apiURL}${buildQueryString({
+                    make,
+                    mileage,
+                    model,
+                    price,
+                    vin,
+                    year,
+                })}`,
+                {
+                    body: formData,
+                    method: "POST",
+                }
+            );
         },
         onSuccess: () => {
             queryClient.invalidateQueries({
-                queryKey: ['cars-for-sale', 'user']
-            })
-        }
-    })
-}
+                queryKey: ["cars-for-sale", "user"],
+            });
+        },
+    });
+};
 
 export const useCarsForSaleUserFilters = () => {
     return {
-        data: { models: ['Mustang', 'F-100'], makes: ['BMW', 'Audi'] },
+        data: { models: ["Mustang", "F-100"], makes: ["BMW", "Audi"] },
         isLoading: false,
-        isFetching: false
-    }
-}
+        isFetching: false,
+    };
+};
 
-export const useCarForSaleDraftDetail = (query: DraftsAPI['GET']['payload']['query']) => {
-    return useQuery<DraftsAPI['GET']['response'], DraftsAPI['GET']['error']>({
-        queryKey: ['cars-for-sale', 'user', 'drafts', query.id],
+export const useCarForSaleDraftDetail = (
+    query: DraftsAPI["GET"]["payload"]["query"]
+) => {
+    return useQuery<DraftsAPI["GET"]["response"], DraftsAPI["GET"]["error"]>({
+        queryKey: ["cars-for-sale", "user", "drafts", query.id],
         queryFn: () => {
-            return apiRequest(draftsApiURL, 'GET')({ query })
-        }
-    })
-}
+            return apiRequest(draftsApiURL, "GET")({ query });
+        },
+        enabled: () => {
+            return !!query.id;
+        },
+    });
+};
 
 export const useCarForSaleDraftCreation = () => {
-    return useMutation<void, DraftsAPI['POST']['error'], Services.CarSaleUser.CreateDraftPayload>({
+    return useMutation<
+        void,
+        DraftsAPI["POST"]["error"],
+        Services.CarSaleUser.CreateDraftPayload
+    >({
         mutationFn: async (variables) => {
-            const formData = new FormData()
+            const formData = new FormData();
 
             if (variables.photos && variables.photos.length) {
                 for (const photo of variables.photos) {
-                    formData.append('photos', photo)
+                    formData.append("photos", photo);
                 }
-
             }
 
-            const { make, mileage, model, price, vin, year } = variables
-            await fetch(`${draftsApiURL}${buildQueryString({ make, mileage, model, price, vin, year })}`, {
-                body: formData,
-                method: "POST"
-            })
-        }
-    })
-}
+            const { make, mileage, model, price, vin, year } = variables;
+            await fetch(
+                `${draftsApiURL}${buildQueryString({
+                    make,
+                    mileage,
+                    model,
+                    price,
+                    vin,
+                    year,
+                })}`,
+                {
+                    body: formData,
+                    method: "POST",
+                }
+            );
+        },
+    });
+};
 
 export const useCarForSaleDraftUpdate = () => {
-    return useMutation<void, DraftsAPI['PATCH']['error'], Services.CarSaleUser.UpdateDraftPayload>({
+    return useMutation<
+        void,
+        DraftsAPI["PATCH"]["error"],
+        Services.CarSaleUser.UpdateDraftPayload
+    >({
         mutationFn: async (variables) => {
-            const formData = new FormData()
+            const formData = new FormData();
 
             if (variables.newPhotos && variables.newPhotos.length) {
                 for (const photo of variables.newPhotos) {
-                    formData.append('photos', photo)
+                    formData.append("photos", photo);
                 }
-
             }
 
-            const { make, mileage, model, price, vin, year } = variables
-            await fetch(`${draftsApiURL}${buildQueryString({ make, mileage, model, price, vin, year })}`, {
-                body: formData,
-                method: "PATCH"
-            })
-        }
-    })
-}
+            console.log("variables", variables);
+
+            const {
+                make,
+                mileage,
+                model,
+                price,
+                vin,
+                year,
+                removedPhotoIds,
+                id,
+            } = variables;
+            await fetch(
+                `${draftsApiURL}${buildQueryString({
+                    make,
+                    mileage,
+                    model,
+                    price,
+                    vin,
+                    year,
+                    removedPhotoIds,
+                    id,
+                })}`,
+                {
+                    body: formData,
+                    method: "PATCH",
+                }
+            );
+        },
+    });
+};
