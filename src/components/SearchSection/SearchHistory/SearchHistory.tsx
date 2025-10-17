882c8d62e8be9219b01d807bd829c2a5f2535ed2
Razmik <razmik.unanyan29@gmail.com>
2025-10-17 15:00:01 -0500

    fixed view

--- src/components/SearchSection/SearchHistory/SearchHistory.tsx
+++ src/components/SearchSection/SearchHistory/SearchHistory.tsx
@@ -66,7 +66,7 @@ export const SearchHistory: FC<SearchHistoryProps> = ({ searches, isLoading }) =
                                                     } else {
                                                         throw new Error("Unexpected response type");
                                                     }
-                                                }}>Report</button> : undefined}
+                                                }}>View Report</button> : undefined}
                                             </div>
                                             <div>
                                                 {data.autocheck ? <button className={styles.viewBtn} onClick={() => {
@@ -76,7 +76,7 @@ export const SearchHistory: FC<SearchHistoryProps> = ({ searches, isLoading }) =
                                                     } else {
                                                         throw new Error("Unexpected response type");
                                                     }
-                                                }}>Report</button> : undefined}
+                                                }}>View Report</button> : undefined}
                                             </div>
                                         </td>
                                     </tr>
