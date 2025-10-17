0326e1c21a831452d78a8f8a2c253335897df70d
alevny <alevnyacow@gmail.com>
2025-10-17 21:05:56 +0400

    Merge branch 'develop' of github.com:do-change-admin/do-change.me into release/1, fixes

---
+++ src/app/report/downloader.tsx
@@ -0,0 +1,51 @@
+// components/PdfDownloader.tsx
+import { FC, useRef } from "react";
+import html2pdf from "html2pdf.js";
+import { Button } from "@mantine/core";
+
+export type Props = {
+    markup: string
+}
+
+export default function PdfDownloader({ markup }: Props) {
+    const iframeRef = useRef<HTMLIFrameElement>(null);
+
+    const handleDownload = () => {
+        const iframe = iframeRef.current;
+        if (!iframe) return;
+
+        const doc = iframe.contentDocument || iframe.contentWindow?.document;
+        if (!doc) return;
+
+        doc.open();
+        doc.write(markup);
+        doc.close();
+
+        iframe.onload = () => {
+            const body = iframe.contentDocument?.body;
+            if (!body) return;
+
+            html2pdf()
+                .set({
+                    margin: 0.5,
+                    filename: "downloaded-document.pdf",
+                    image: { type: "jpeg", quality: 0.98 },
+                    html2canvas: { scale: 2 },
+                    jsPDF: { unit: "in", format: "a4", orientation: "portrait" },
+                })
+                .from(body)
+                .save();
+        };
+    };
+
+    return (
+        <>
+            <Button onClick={handleDownload}>📄 Download PDF</Button>
+            <iframe
+                ref={iframeRef}
+                style={{ display: "none" }}
+                title="pdf-generator"
+            />
+        </>
+    );
+}
--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import styles from  './page.module.css';
+import styles from './page.module.css';
+import { Button } from "@mantine/core";
+import PdfDownloader from "./downloader";
+import { useRouter } from "next/navigation";
 
 export default function ReportPage() {
     const [reportHtml, setReportHtml] = useState<string | null>(null);
+    const router = useRouter()
 
     useEffect(() => {
         const html = sessionStorage.getItem("report");
@@ -14,6 +18,13 @@ export default function ReportPage() {
     if (!reportHtml) return <p>Loading report...</p>;
 
     return (
-        <div className={styles.container} dangerouslySetInnerHTML={{ __html: reportHtml! }} />
+        <div>
+            <div style={{ display: 'flex', justifyContent: 'center', gap: '16px', marginTop: '24px' }}>
+                <PdfDownloader markup={reportHtml!} />
+                <Button variant="light" onClick={() => { router.back() }}>Go back</Button>
+            </div>
+
+            <div className={styles.container} dangerouslySetInnerHTML={{ __html: reportHtml! }} />
+        </div>
     );
 }
