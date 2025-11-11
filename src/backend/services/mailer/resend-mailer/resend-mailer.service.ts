890b03a1271439d07c9ac598f2a764c3af8f5f5b
Maksim Rylov <mksm.rlv.tb@gmail.com>
2025-11-11 17:40:32 +0300

    test

--- src/backend/services/mailer/resend-mailer/resend-mailer.service.ts
+++ src/backend/services/mailer/resend-mailer/resend-mailer.service.ts
@@ -8,19 +8,22 @@ export class ResendMailerService implements IMailerService {
     private sender!: ResendSender;
 
     constructor() {
-        console.log("RESEND_API_KEY exists:", !!process.env.RESEND_API_KEY);
+        console.log("RESEND_API_KEY exists:", process.env.RESEND_API_KEY);
         this.sender = new ResendSender(process.env.RESEND_API_KEY!);
     }
 
     async send(emailMessage: EmailMessage): Promise<void> {
         const { message } = emailMessage;
 
-        const { error } = await this.sender.emails.send({
+        const { error, data } = await this.sender.emails.send({
             from: message.from,
             to: message.to,
             subject: message.subject,
             html: message.content,
         });
+
+        console.log("MAIL data", data);
+        console.log("MAIL error", error);
 
         if (error) {
             console.log("ERRORORORORORO", error);
