203c625df85ebd2f1426e14a486b11d09e5d8795
Maksim Rylov <mksm.rlv.tb@gmail.com>
2025-11-12 20:18:44 +0300

    move mailer from services to providers

--- src/backend/di-containers/index.ts
+++ src/backend/di-containers/index.ts
@@ -3,14 +3,14 @@ import "reflect-metadata";
 import { testContainer } from "./test.di-container";
 import { devContainer } from "./dev.di-container";
 import { stageContainer } from "./stage.di-container";
-import { ControllerTokens, ServiceTokens } from "./tokens.di-container";
+import { ControllerTokens, ProviderTokens } from "./tokens.di-container";
 import type { SyndicationRequestsController } from "@/backend/controllers/syndication-requests.controller";
 import type { SyndicationRequestDraftsController } from "@/backend/controllers/syndication-request-drafts.controller";
 import type { SyndicationRequestManagementController } from "@/backend/controllers/syndication-request-management.controller";
 import type { UserNotificationsController } from "../controllers/user-notifications.controller";
 import type { UserNotificationsManagementController } from "../controllers/user-notifications-management.controller";
 import type { SubscriptionsController } from "../controllers/subscriptions.controller";
-import { IMailerService } from "../services/mailer/mailer.service";
+import { IMailerProvider } from "../providers/mailer/mailer.provider";
 
 const getDIContainer = () => {
     if (process.env["IN_TEST"]) {
@@ -69,8 +69,8 @@ export const DIContainer = () => {
                 ControllerTokens.subscriptions
             );
         },
-        MailerService: () => {
-            return container.get<IMailerService>(ServiceTokens.email);
+        MailerProvider: () => {
+            return container.get<IMailerProvider>(ProviderTokens.mailer);
         },
     };
 };
