f26528747c51e5099db144a54de89aab2482e2c8
alevny <alevnyacow@gmail.com>
2025-11-25 16:04:28 +0400

    removed unused code

--- src/client/components/Header/Header.tsx
+++ src/client/components/Header/Header.tsx
@@ -1,21 +1,11 @@
 "use client";
 
 import styles from "./Header.module.css";
-import {Text, Box, Group, Image, Tooltip, ActionIcon, Indicator, Drawer} from "@mantine/core";
-import { LanguageSelector } from "@/client/components";
-import { usePathname } from "next/navigation";
+import { Image } from "@mantine/core";
 import React from "react";
-import { FaBell } from "react-icons/fa6";
-import {useUnreadNotificationsCount} from "@/client/hooks";
-import {useDisclosure} from "@mantine/hooks";
 import { UserListWidget } from "@/client/widgets/notifications/user-list/user-list.widget";
-// import {UserNotifications} from "@/client/features/user-notifications";
 
 export const Header = () => {
-    const pathname = usePathname();
-    const {count, isFetching} = useUnreadNotificationsCount()
-    const [opened, { open, close }] = useDisclosure(false);
-
     return (
         <header className={styles.header}>
             <Image
