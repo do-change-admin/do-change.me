dffd737bd7be556abb1dfb0285881bebf3a97402
Maksim Rylov <mksm.rlv.tb@gmail.com>
2025-11-26 20:31:19 +0300

    updated di

--- src/backend/di-containers/stage.di-container.ts
+++ src/backend/di-containers/stage.di-container.ts
@@ -1,50 +1,44 @@
-import { DataProviders, DataProvidersImplemetations, FunctionProviders, FunctionProvidersImplementations } from '@/backend/providers'
-import { Container } from 'inversify'
-import { StoreTokens, ProviderTokens } from './tokens.di-container'
-import { registerServices } from './register-services'
-import { registerControllers } from './register-controllers'
-import { NotificationStore } from '../stores/notification/notification.store'
-import { NotificationRAMStore } from '../stores/notification/notification.ram.store'
+import { Container } from 'inversify';
+import {
+    type DataProviders,
+    DataProvidersImplemetations,
+    type FunctionProviders,
+    FunctionProvidersImplementations
+} from '@/backend/providers';
+import { NotificationPrismaStore } from '../stores/notification/notification.prisma.store';
+import type { NotificationStore } from '../stores/notification/notification.store';
+import { registerControllers } from './register-controllers';
+import { registerServices } from './register-services';
+import { ProviderTokens, StoreTokens } from './tokens.di-container';
 
-const container = new Container()
+const container = new Container();
 
 const registerDataProviders = () => {
     container
         .bind<DataProviders.SyndicationRequests.Interface>(StoreTokens.syndicationRequests)
-        .to(DataProvidersImplemetations.Prisma.SyndicationRequests)
+        .to(DataProvidersImplemetations.Prisma.SyndicationRequests);
     container
         .bind<DataProviders.SyndicationRequestDrafts.Interface>(StoreTokens.syndicationRequestDrafts)
-        .to(DataProvidersImplemetations.Prisma.SyndicationRequestDrafts)
+        .to(DataProvidersImplemetations.Prisma.SyndicationRequestDrafts);
     container
         .bind<DataProviders.VehicleHistoryReports.Interface>(StoreTokens.vehicleHistoryReports)
-        .to(DataProvidersImplemetations.API.VehicleHistoryReports)
+        .to(DataProvidersImplemetations.API.VehicleHistoryReports);
     container
         .bind<DataProviders.VehicleHistoryReports.CacheInterface>(StoreTokens.vehicleHistoryReportsCache)
-        .to(DataProvidersImplemetations.Prisma.VehicleHistoryReportsCache)
-    container
-        .bind<DataProviders.Pictures.Interface>(StoreTokens.pictures)
-        .to(DataProvidersImplemetations.API.PicturesInVercelBlob)
-    container
-        .bind<NotificationStore>(StoreTokens.notifications)
-        .to(NotificationRAMStore).inSingletonScope()
-}
+        .to(DataProvidersImplemetations.Prisma.VehicleHistoryReportsCache);
+    container.bind<DataProviders.Pictures.Interface>(StoreTokens.pictures).to(DataProvidersImplemetations.API.PicturesInVercelBlob);
+    container.bind<NotificationStore>(StoreTokens.notifications).to(NotificationPrismaStore);
+};
 
 const registerFunctionProviders = () => {
-    container
-        .bind<FunctionProviders.Email.Interface>(ProviderTokens.email)
-        .to(FunctionProvidersImplementations.Mock.Email)
+    container.bind<FunctionProviders.Email.Interface>(ProviderTokens.email).to(FunctionProvidersImplementations.Mock.Email);
 
-    container
-        .bind<FunctionProviders.Logger.Interface>(ProviderTokens.logger)
-        .to(FunctionProvidersImplementations.Mock.Logger)
+    container.bind<FunctionProviders.Logger.Interface>(ProviderTokens.logger).to(FunctionProvidersImplementations.Mock.Logger);
+};
 
-}
+registerDataProviders();
+registerFunctionProviders();
+registerServices(container);
+registerControllers(container);
 
-
-registerDataProviders()
-registerFunctionProviders()
-registerServices(container)
-registerControllers(container)
-
-
-export { container as stageContainer }
+export { container as stageContainer };
