10c3774c532ceb2a2ccd64641afef00dbe02c2b2
m.rylov <mksm.rilov@gmail.com>
2026-01-21 17:47:59 +0300

    fix

--- src/value-objects/_exports.ts
+++ src/value-objects/_exports.ts
@@ -1,3 +1,2 @@
 // export * as Pagination from './pagination.value-object'
-export * as StringHash from "./string-hash.value-object";
-export * as Picture from "./picture.value-object";
+export * as Picture from './picture.value-object';
